06e08f38bafac6b4c3e76f21ed7933bfc3646cc2
Josh Brown <joshcarlbrown@gmail.com>
2024-02-25 10:57:31 +1100

    accept client args from command line

--- rallpaper-client/src/main.rs
+++ rallpaper-client/src/main.rs
@@ -1,9 +1,23 @@
+use std::env;
 use std::io::{Read, Write};
 use std::os::unix::net::UnixStream;
 
+const HELP_MESSAGE: &str = "usage: rallpaper-client [change|toggle|time]";
+
 fn main() {
+    let command: String = env::args().nth(1).expect(HELP_MESSAGE);
+    if ["change", "toggle", "time"]
+        .into_iter()
+        .all(|accepted_command| accepted_command != &command)
+    {
+        println!("{}", HELP_MESSAGE);
+        std::process::exit(1)
+    }
+
     let mut conn = UnixStream::connect("/tmp/rallpaper.sock").unwrap();
-    write!(conn, "close").unwrap();
+    write!(conn, "{}", command).unwrap();
     conn.shutdown(std::net::Shutdown::Write).unwrap();
-    conn.read_to_end(&mut Vec::new()).unwrap();
+    let mut response = String::new();
+    conn.read_to_string(&mut response).unwrap();
+    println!("received: {}", response);
 }
