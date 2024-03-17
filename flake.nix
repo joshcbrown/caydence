8eff1842c98fb8ed097822d16544b1048050754d
Josh Brown <joshcarlbrown@gmail.com>
2024-03-17 14:18:56 +1100

    add flake

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "caydence devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              pkg-config
+              libnotify
+              glib
+              gtk-pixbuf
+              rust-bin.beta.latest.default
+            ];
+
+            shellHook = ''
+              alias ls=eza
+              alias find=fd
+            '';
+          };
+        }
+    );
+}
