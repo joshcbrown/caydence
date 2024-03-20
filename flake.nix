f5bbcca4ad69746509a35a9f620b964cb8c97637
Josh Brown <joshcarlbrown@gmail.com>
2024-03-20 19:29:58 +1100

    add package to flake

--- flake.nix
+++ flake.nix
@@ -4,12 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
+    crane,
     rust-overlay,
     flake-utils,
     ...
@@ -20,16 +25,27 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+
+        craneLib = crane.lib.${system};
+
+        crate = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+          strictDeps = true;
+
+          buildInputs = with pkgs; [
+            pkg-config
+            libnotify
+            glib
+            gdk-pixbuf
+            rust-bin.beta.latest.default
+          ];
+        };
       in
         with pkgs; {
+          packages.default = crate;
+          checks = {inherit crate;};
           devShells.default = mkShell {
-            buildInputs = [
-              pkg-config
-              libnotify
-              glib
-              gtk-pixbuf
-              rust-bin.beta.latest.default
-            ];
+            inputsFrom = [crate];
 
             shellHook = ''
               alias ls=eza
